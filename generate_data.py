31640fe992631802300efbdc53a0ef4a84cedff3
Thanh Cao <caoduythanhcantho@gmail.com>
2022-02-13 00:15:35 +0700

    Fix visualize imgs and boxes

--- generate_data.py
+++ generate_data.py
@@ -11,23 +11,16 @@ def parse_args():
     parser.add_argument('--imagespath', default='../Table_Detection_Dataset/unlv/train/images', help='Directory containing UNLV dataset images.')
     parser.add_argument('--ocrpath', default='../Table_Detection_Dataset/unlv/unlv_xml_ocr', help='Directory containing ground truths of characters in UNLV dataset.')
     parser.add_argument('--tablepath', default='../Table_Detection_Dataset/unlv/unlv _xml_gt', help='Directory containing ground truths of tables in UNLV dataset.')
-    parser.add_argument('--visualizeimgs', type=int, default=0, help='Store the generated images (along than tfrecords). Default: 0.')
-    parser.add_argument('--visualizebboxes', type=int, default=0, help='Store the images with bound boxes. Default: 0.')
+    parser.add_argument('--visualizeimgs', type=int, action='store_true', help='Store the generated images (along than tfrecords).')
+    parser.add_argument('--visualizebboxes', type=int, action='store_true', help='Store the images with bound boxes.')
     return parser.parse_args()
 
 
 def run_generator(args):
-    filesize = max(int(args.filesize), 4)
-    visualizeimgs = False
-    if (args.visualizeimgs == 1):
-        visualizeimgs = True
-
-    visualizebboxes = False
-    if (args.visualizebboxes == 1):
-        visualizebboxes = True
+    file_size = max(int(args.filesize), 4)
     distributionfile = 'unlv_distribution'
-    t = GenerateTFRecord(args.outpath, args.num_trecords, filesize, args.imagespath,
-                         args.ocrpath, args.tablepath, visualizeimgs, visualizebboxes, distributionfile)
+    t = GenerateTFRecord(args.outpath, args.num_trecords, file_size, args.imagespath,
+                         args.ocrpath, args.tablepath, args.visualizeimgs, args.visualizebboxes, distributionfile)
     t.write_to_tf(args.threads)
 
 
